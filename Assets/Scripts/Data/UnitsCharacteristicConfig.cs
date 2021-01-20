4dc5ac07e83da1397fd2cffb1fb5fab5f988572a
Juan Diaz <juan.rod.diaz@gmail.com>
2021-01-20 10:47:16 +0100

    Adding scenes and basic script folder hierarchy. Adds unity stats config with data as the test requires.

---
+++ Assets/Scripts/Data/UnitsCharacteristicConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class CharacteristicModifier
+{
+    public UnitMainStats Stat;
+    public int Mod;
+}
+
+[Serializable]
+public class ShapeModifier
+{
+    public UnitShape Shape;
+    public List<CharacteristicModifier> Modifier;
+}
+
+[Serializable]
+public class SizeModifier
+{
+    public UnitSize Size;
+    public CharacteristicModifier Modifier;
+}
+
+[Serializable]
+public class ColourModifier
+{
+    public UnitColour Colour;
+    public CharacteristicModifier Modifier;
+}
+
+[Serializable]
+public class ShapeAndColourModifier
+{
+    public UnitShape Shape;
+    public ColourModifier ColourModifier;
+}
+
+
+[CreateAssetMenu(menuName = "My Assets/Units Characteristic Config")]
+public class UnitsCharacteristicConfig : ScriptableObject
+{
+    public List<ShapeModifier> ShapeModifiersList = new List<ShapeModifier>();
+    public List<SizeModifier> SizeModifiersList = new List<SizeModifier>();
+    public List<ShapeAndColourModifier> ShapeAndColourModifiersList = new List<ShapeAndColourModifier>();
+
+}
