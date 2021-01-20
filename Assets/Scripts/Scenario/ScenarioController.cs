fea5703c42d855e1b099e091df9a581b91542911
Juan Diaz <juan.rod.diaz@gmail.com>
2021-01-20 12:37:42 +0100

    Arranging the stage to show the battlefields, creating first logic hooks from controllers to views.

---
+++ Assets/Scripts/Scenario/ScenarioController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioController : MonoBehaviour
+{
+    [SerializeField]
+    private UnitsGridLogic _gridLogic;
+    [Header("Config")]
+    private UnitsCharacteristicConfig _unitsCharacteristicConfig;
+
+    private UnitFactory _factory = new UnitFactory();
+
+    private void Start()
+    {
+        
+    }
+}
+
---
+++ Assets/Scripts/Units/UnitFactory.cs
@@ -0,0 +1,22 @@
+﻿
+public class UnitFactory
+{
+    private UnitsCharacteristicConfig _config;
+    private bool _initialized = false;
+
+    public void Initialize(UnitsCharacteristicConfig config)
+    {
+        _config = config;
+        _initialized = true;
+    }
+
+    public UnitLogic CreateRandomUnit()
+    {
+        if (!_initialized)
+        {
+            return null;
+        }
+
+        return null;
+    }
+}
